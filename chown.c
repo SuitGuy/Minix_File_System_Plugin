c3fece2170dec904d8b606a7566445f8079754d8
SuitGuy <pjdines1994@gmail.com>
2015-12-10 05:04:06 +0000

    altered minux_chown to work for the proccess chown method and added comments to follow workflow

--- chown.c
+++ chown.c
@@ -15,9 +15,22 @@ sets uid-field for given inode to specified value
 
 */
 
-void minix_chown(struct super_block * sb, int ino,struct path path, int uid){
+void minix_chown( int ino,char* filepath, int uid){
 	struct inode * i_node;
+	struct dentry *dentry;
+	struct path path;
+	struct super_block *sb;
 
+	//do not alter the root inode
+	if(ino == 0 ){
+		return;
+	}
+	printk(KERN_INFO "MINIX CHOWN CALLED WITH ino=%i , uid= %i\n", ino , uid);
+	//aquire the dentry and super block from the path
+
+	kern_path(filepath, LOOKUP_FOLLOW, &path);
+	dentry = path.dentry;
+	sb = dentry->d_sb;
 
 	// aquire inode
 	i_node = minix_iget(sb, ino);
@@ -31,6 +44,8 @@ void minix_chown(struct super_block * sb, int ino,struct path path, int uid){
 	//return inode to VFS
 	iput(i_node);
 	
+	//return the path to the VFS
+	path_put(&path);
 
 	return;
 }
@@ -42,7 +57,10 @@ void minix_chown(struct super_block * sb, int ino,struct path path, int uid){
  */
 
 
-int process_inodes(struct super_block *sb, int uid){
+int process_inodes(char * filepath, int uid){
+	struct dentry *dentry;
+	struct path path;
+	struct super_block *sb;
 	struct minix_sb_info *sbi;
 	struct buffer_head ** s_imap;
 	int index = 0;
@@ -54,42 +72,61 @@ int process_inodes(struct super_block *sb, int uid){
 	u32 bits;
 	int *p;
 
-	printk(KERN_INFO "ENTERING PROCESSINODE \n");
+	//extract the super block from the path dentry
+	kern_path(filepath, LOOKUP_FOLLOW, &path);
+	dentry = path.dentry;
+	sb = dentry->d_sb;
+
+	//extract super block info to aquire inode bitmap
 	sbi = minix_sb(sb);
 	s_imap = sbi->s_imap;
+	
+	//calculate the number of blocks to check
 	bits = sbi->s_ninodes + 1;
 	blocks = DIV_ROUND_UP(bits, sb->s_blocksize * 8);
+
+	//calculate the number of valid inodes you should be looking for
 	totalvalidinodes = sbi->s_ninodes - minix_count_free_inodes(sb);
 	printk(KERN_INFO "NO OF VALID INODES %i \n" , totalvalidinodes);
-	while (blocks-- && (inodesfound > totalvalidinodes)) {
+
+	//for all the blocks you have
+	while (blocks-- && (inodesfound < totalvalidinodes)) {
 		unsigned words = sb->s_blocksize / 2;
 		p = (int *)(*s_imap++)->b_data;
-		while (words-- && (inodesfound > totalvalidinodes)){
-			if(inodesfound > totalvalidinodes){
-					break;
-				}
+
+		//for all the words in the block
+		while (words-- && (inodesfound < totalvalidinodes)){
 				printk(KERN_INFO "p %i \n", *p);
 
-			while (index < 16 && (inodesfound > totalvalidinodes)){
+			//for all of the bits in that word
+			while (index < 16 && (inodesfound < totalvalidinodes)){
+				printk(KERN_INFO "CURRENT INODE %i\n", curino);
+				//do bitwise comparison to check if bit is valid inode				
 				if(curino < 16 &&(*p & comparisonint)!= 0){
 					printk(KERN_INFO "INODE %i VALID\n", curino);
+					
+					//call minix_chown on valid inode
+					minix_chown(curino, filepath, uid);
 					inodesfound ++;
 				}
 				printk(KERN_INFO "COMPARISON INT %i\n", comparisonint);
-				comparisonint = comparisonint *2 ;
+				//perform a right bitshift by 1 essentialy multiplying by 2				
+				comparisonint = comparisonint >> 1 ;
 				
+				//increment current inode and index of next bit
 				curino ++;
 				index++;
 			}
 			comparisonint = 1;
+			//look at the next word in the block.
 			p++;
 			index = 0;
 		}
-		if(inodesfound > totalvalidinodes){
-			break;
-		}
 		
 	}
+
+	//return the path to the VFS
+	path_put(&path);
 	
 	return 0;
 }
@@ -101,30 +138,19 @@ int process_inodes(struct super_block *sb, int uid){
  */
 
 int proc_chown(int ino, char * filepath, int uid){
-	
+
 		
-
-	struct dentry *dentry;
-	struct path path;
-	struct super_block *sb;
-
-	printk(KERN_INFO "PROC CHOWN %i %i %s", ino, uid, filepath);
-
-	kern_path(filepath, LOOKUP_FOLLOW, &path);
-	dentry = path.dentry;
-	sb = dentry->d_sb;
-	process_inodes(sb, uid);
-/*
-	if(ino >0){
-		minix_chown(sb, ino ,path, uid);
+	if(ino > 0){
+		//change the ownership for the specified inode
+		minix_chown(ino,filepath,uid);
+	}else if(ino == 0){
+		//change the ownership for all inodes in the VFS
+		process_inodes(filepath, uid);
 	}else{
-		
-	}*/
-	path_put(&path);	
-	/*else{
-		process_inodes(sb, uid); 
+		//invalid inode number supplied.
+		return -EFAULT;
 	}
-*/
+	
 	return 0;
 	
 	
